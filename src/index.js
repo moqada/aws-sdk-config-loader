e83cdc081069ef0cb911e1cd7139e29d1fe369dc
moqada <moqada@gmail.com>
2015-09-23 02:22:07 +0900

    feat: Add noReflectCredentials option

--- src/index.js
+++ src/index.js
@@ -8,11 +8,29 @@ import configProvider from './configProvider';
  * @param {Object} AWS AWS SDK module
  * @param {Object} opts options
  * @param {string} opts.profile profile name
+ * @param {boolean} opts.noReflectCredentials opts.profile doesnot reflect credentials
  */
 export default function loader(AWS, opts = {}) {
   AWS.util.update(AWS.Config.prototype.keys, configProvider(AWS, [
     fileConfigs,
     environmentConfigs
   ], opts));
+  if (!opts.noReflectCredentials) {
+    AWS.util.update(AWS.Config.prototype.keys, {
+      credentials: () => {
+        let credentials = null;
+        new AWS.CredentialProviderChain([
+          () => new AWS.EnvironmentCredentials('AWS'),
+          () => new AWS.EnvironmentCredentials('AMAZON'),
+          () => new AWS.SharedIniFileCredentials({profile: opts.profile})
+        ]).resolve((err, creds) => {
+          if (!err) {
+            credentials = creds;
+          }
+        });
+        return credentials;
+      }
+    });
+  }
   AWS.config = new AWS.Config();
 }
--- test/loader-spec.js
+++ test/loader-spec.js
@@ -1,33 +1,32 @@
+import path from 'path';
+
 import AWS from 'aws-sdk';
 import sinon from 'sinon';
 import assert from 'power-assert';
 import proxyquire from 'proxyquire';
 
 import configProvider from '../src/configProvider';
-import {resetEnv} from './helpers';
+import environmentConfigs from '../src/configs/environmentConfigs';
+import fileConfigs from '../src/configs/fileConfigs';
+import {resetEnv, fixtureDir} from './helpers';
 
 
 describe('aws-sdk-config-loader', () => {
-  let orgConfig = null;
+  let orgConfigKeys = null;
   let loader = null;
   let sandbox = null;
   let stubs = null;
 
   beforeEach(done => {
     resetEnv();
-    orgConfig = AWS.config;
+    process.env.HOME = path.join(fixtureDir(), 'default');
+    orgConfigKeys = Object.assign({}, AWS.Config.prototype.keys);
     sandbox = sinon.sandbox.create();
     stubs = {
-      configProvider: sandbox.spy(configProvider),
-      fileConfigs: config => {
-        return Object.assign({}, config, {region: 'ap-northeast-1'});
-      },
-      environmentConfigs: config => config
+      configProvider: sandbox.spy(configProvider)
     };
     loader = proxyquire('../src', {
-      './configProvider': stubs.configProvider,
-      './configs/fileConfigs': stubs.fileConfigs,
-      './configs/environmentConfigs': stubs.environmentConfigs
+      './configProvider': stubs.configProvider
     });
     done();
   });
@@ -36,32 +35,68 @@ describe('aws-sdk-config-loader', () => {
     resetEnv();
     sandbox.restore();
     stubs = null;
-    AWS.config = orgConfig;
+    AWS.Config.prototype.keys = orgConfigKeys;
     done();
   });
 
-  it('override AWS.config', done => {
+  it('override AWS.config', () => {
     loader(AWS);
     assert(stubs.configProvider.args.length === 1);
     assert.deepEqual(stubs.configProvider.args[0], [
       AWS,
-      [stubs.fileConfigs, stubs.environmentConfigs],
+      [fileConfigs, environmentConfigs],
       {}
     ]);
     assert(AWS.config.region === 'ap-northeast-1');
-    done();
+    assert(AWS.config.credentials.accessKeyId === 'default_key_id');
+    assert(AWS.config.credentials.secretAccessKey === 'default_secret_key');
   });
 
-  it('override AWS.confg by options', done => {
-    const opts = {profile: 'buz'};
-    loader(AWS, opts);
-    assert(stubs.configProvider.args.length === 1);
-    assert.deepEqual(stubs.configProvider.args[0], [
-      AWS,
-      [stubs.fileConfigs, stubs.environmentConfigs],
-      opts
-    ]);
-    assert(AWS.config.region === 'ap-northeast-1');
-    done();
+  context('when apply options', () => {
+    it('override config and credentials', () => {
+      const opts = {profile: 'customize'};
+      loader(AWS, opts);
+      assert(stubs.configProvider.args.length === 1);
+      assert.deepEqual(stubs.configProvider.args[0], [
+        AWS,
+        [fileConfigs, environmentConfigs],
+        opts
+      ]);
+      assert(AWS.config.region === 'us-east-1');
+      assert(AWS.config.credentials.accessKeyId === 'customize_key_id');
+      assert(AWS.config.credentials.secretAccessKey === 'customize_secret_key');
+    });
+
+    it('and apply noReflectCredentials override only config', () => {
+      const opts = {
+        profile: 'customize',
+        noReflectCredentials: true
+      };
+      loader(AWS, opts);
+      assert(stubs.configProvider.args.length === 1);
+      assert.deepEqual(stubs.configProvider.args[0], [
+        AWS,
+        [fileConfigs, environmentConfigs],
+        opts
+      ]);
+      assert(AWS.config.region === 'us-east-1');
+      assert(AWS.config.credentials.accessKeyId === 'default_key_id');
+      assert(AWS.config.credentials.secretAccessKey === 'default_secret_key');
+    });
+
+    it('and apply noReflectCredentials and throw error', () => {
+      const opts = {
+        profile: 'customize'
+      };
+      class StubCredentials {
+        get(cb) {
+          cb('err');
+        }
+      }
+      sandbox.stub(AWS, 'SharedIniFileCredentials', StubCredentials);
+      loader(AWS, opts);
+      assert(AWS.config.region === 'us-east-1');
+      assert(AWS.config.credentials === null);
+    });
   });
 });
